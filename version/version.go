26212138d1dae4d633ff111fa1bb33c86ada1847
chenqiang <ichenq@outlook.com>
2018-07-13 14:19:38 +0800

    Add version

---
+++ version/version.go
@@ -0,0 +1,7 @@
+// Copyright (C) 2018-present ichenq@outlook.com. All rights reserved.
+// Distributed under the terms and conditions of the Apache License.
+// See accompanying files LICENSE.
+
+package version
+
+var Version = "1.0.1"
